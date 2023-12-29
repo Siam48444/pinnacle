20f4effbcb77a3f5759c1fdbef418342d34c118d
Siam48444 <siam48444@gmail.com>
2023-12-29 14:06:29 +0600

    jj

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 var old_Y = window.scrollY;
 
 window.addEventListener("scroll", () => {
-   if (old_Y < window.scrollY) {
-      gsap.to("nav", {
-         y: "-100%",
-         duration: 0.5,
-         ease: "Power5.easeInOut",
-      });
-   } else {
-      gsap.to("nav", {
-         y: 0,
-         duration: 0.5,
-         ease: "Power5.easeInOut",
-      });
-   }
+   old_Y < window.scrollY ? scrollingdDown() : scrollingdUp();
    old_Y = window.scrollY;
 });
+
+function scrollingdDown() {
+   gsap.to("nav", {
+      y: "-100%",
+      duration: 0.5,
+      ease: "Power5.easeInOut",
+   });
+}
+function scrollingdUp() {
+   gsap.to("nav", {
+      y: 0,
+      duration: 0.5,
+      ease: "Power5.easeInOut",
+   });
+}
