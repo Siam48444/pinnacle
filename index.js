231c59d451ac7db62bd56e54e24f9d4bf3935720
Siam48444 <siam48444@gmail.com>
2023-12-29 19:59:04 +0600

    hh

--- index.js
+++ index.js
@@ -48,18 +48,3 @@ orangeShutters.forEach((shutter) => {
       },
    });
 });
-
-// Swiper JS
-var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 1,
-   spaceBetween: 30,
-   loop: true,
-   pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-   },
-   navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-   },
-});
